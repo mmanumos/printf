063992675f499de9f30d34fded10c56ca2487ff3
mmanumos <mosqueramanuel5@gmail.com>
2019-10-29 06:58:40 +0000

    [Fix] Fixed styles for Betty modified:   printf.c

--- printf.c
+++ printf.c
@@ -1,14 +1,11 @@
 #include "holberton.h"
 
 /**
- *
- *
- */
-
-/**
  * _printf - get the right function to call
  * and send the variables and after write the
  * concatenated buffer
+ *
+ * @format: list with arguments
  *
  * Return: Total length of string
  *
@@ -16,13 +13,10 @@
 
 int _printf(const char *format, ...)
 {
-
-	int i, k, bi = 0;
+	int i = 0, k, bi = 0;
 	va_list par;
 	char *buf = malloc(1024);
 	char *cad;
-
-
 	sp print_f[] = {
 		{"s", _get_string},
 		{"c", _get_char},
@@ -31,16 +25,12 @@ int _printf(const char *format, ...)
 		{NULL, NULL}
 	};
 
-
 	va_start(par, format);
 
-	i = 0;
 	while (format[i] && format)
 	{
-
-		if ((format[i] == '%') && (format[i+1] != '%'))
+		if ((format[i] == '%') && (format[i + 1] != '%'))
 		{
-
 			k = 0;
 			while (print_f[k].flag)
 			{
@@ -52,22 +42,14 @@ int _printf(const char *format, ...)
 				}
 				k++;
 			}
-
 			i++;
 		}
-
-		else
-		{
-			buf[bi] =  format[i];
-			bi++;
-		}
-
+		buf[bi] =  format[i];
+		bi++;
 		i++;
-
 	}
 
 	buf[bi + 1] = '\0';
-	write(1,buf,_strlen(buf));
+	write(1, buf, _strlen(buf));
 	return (_strlen(buf));
-
 }
