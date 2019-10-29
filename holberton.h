6b305e154b97abb994dc7222283357b299418e1b
mmanumos <mosqueramanuel5@gmail.com>
2019-10-29 06:03:12 +0000

    [Feature] Definition of structure and initial protoypes functions

--- holberton.h
+++ holberton.h
@@ -1,4 +1,32 @@
 #ifndef HOLBERTON_H_
 #define HOLBERTON_H_
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * struct specifiers - Struct op
+ *
+ * @flag: Flag to get function
+ * @f: The function associated (address)
+ */
+
+typedef struct specifiers
+{
+	char *flag;
+	char *(*f)();
+}sp;
+
+
+
+
 int _printf(const char *format, ...);
+
+char *_strncat(char *dest, char *src, int n);
+int _strlen(char *s);
+char *_get_char(va_list cad);
+char *_get_string(va_list cad);
+char *get_number(va_list integer);
+
 #endif
