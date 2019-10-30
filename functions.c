cc4ace0cea8e0b4339933bbb581ed8e130cd65ef
AdnxCHdz <1214@holbertonschool.com>
2019-10-29 19:58:13 -0500

    [Feature]: Adding new prototype function modified:   functions.c

--- functions.c
+++ functions.c
@@ -148,3 +148,31 @@ char *get_number(va_list integer)
 	}
 	return (cad);
 }
+/**
+ * _calloc - prints buffer in hexa
+ * @nmemb: the number of elements that will be in the string
+ * @size: the size of the type of data
+ * Return: dimensions of the string
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *isize;
+	unsigned int a = 0;
+	unsigned int mul = nmemb * size;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	isize = (void *)malloc(mul);
+	if (isize == NULL)
+	{
+		return (NULL);
+	}
+	while (a < mul)
+	{
+		*(isize + a) = 0;
+		a++;
+	}
+	return (isize);
+}
